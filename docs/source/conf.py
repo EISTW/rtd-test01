c1f1575b0969b1eca5848164613a711ac29311db
EISTW <107044604+EISTW@users.noreply.github.com>
2022-11-01 20:41:50 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,5 +30,14 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
+# The _static folder must be in the docs folder. Files here are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+# A list of CSS files. The entry must be a filename string or a tuple containing
+# the filename string and the attributes dictionary. The filename must be
+# relative to the html_static_path, or a full URI
+html_css_files = ["custom.css"]
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
